fc521188c941e2d71109d89f935c2cc30daf17ee
AdamZhou1029 <yizhouzhou@yizhous-MacBook-Pro.local>
2022-06-25 16:40:09 -0500

    environment variable

--- controllers/sts.js
+++ controllers/sts.js
@@ -1,55 +1,56 @@
 const Core = require("@alicloud/pop-core");
 
 const OSS = require("ali-oss");
+require("dotenv").config();
 
 exports.getDownloadLink = async (req, res) => {
-    const {filename} = req.body;
-    const client = new OSS({
-        // yourRegion填写Bucket所在地域。以华东1（杭州）为例，yourRegion填写为oss-cn-hangzhou。
-        region: "oss-cn-hangzhou",
-        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        accessKeyId: "LTAI5tCbNAQSZ2uAaz2jFtLk",
-        accessKeySecret: "H4evE0nAeJpkXsO6Q142Ssk4952ZAO",
-        // 填写Bucket名称。
-        bucket: "qrcode-portal",
-    });
+  const { filename } = req.body;
+  const client = new OSS({
+    // yourRegion填写Bucket所在地域。以华东1（杭州）为例，yourRegion填写为oss-cn-hangzhou。
+    region: "oss-cn-hangzhou",
+    // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+    accessKeyId: process.env.ALIYUN_ACCESS_KEY_ID,
+    accessKeySecret: process.env.ALIYUN_ACCESS_KEY_SECRET,
+    // 填写Bucket名称。
+    bucket: "qrcode-portal",
+  });
 
-    // 获取下载exampleobject.txt文件的签名URL，使用浏览器访问时默认直接预览要下载的文件。
-    // 填写不包含Bucket名称在内的Object完整路径。
-    const url = client.signatureUrl("cert-image/" + filename, { expires: 180 });
-    res.status(200).json({
-        message: "get link successful",
-        data: url,
-    });
+  // 获取下载exampleobject.txt文件的签名URL，使用浏览器访问时默认直接预览要下载的文件。
+  // 填写不包含Bucket名称在内的Object完整路径。
+  const url = client.signatureUrl("cert-image/" + filename, { expires: 180 });
+  res.status(200).json({
+    message: "get link successful",
+    data: url,
+  });
 };
 
 exports.getUploadLink = async (req, res) => {
-    const { filename, mimeString } = req.body;
-    const client = new OSS({
-        // yourRegion填写Bucket所在地域。以华东1（杭州）为例，yourRegion填写为oss-cn-hangzhou。
-        region: "oss-cn-hangzhou",
-        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        accessKeyId: "LTAI5tCbNAQSZ2uAaz2jFtLk",
-        accessKeySecret: "H4evE0nAeJpkXsO6Q142Ssk4952ZAO",
-        // 填写Bucket名称。
-        bucket: "qrcode-portal",
-    });
+  const { filename, mimeString } = req.body;
+  const client = new OSS({
+    // yourRegion填写Bucket所在地域。以华东1（杭州）为例，yourRegion填写为oss-cn-hangzhou。
+    region: "oss-cn-hangzhou",
+    // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+    accessKeyId: process.env.ALIYUN_ACCESS_KEY_ID,
+    accessKeySecret: process.env.ALIYUN_ACCESS_KEY_SECRET,
+    // 填写Bucket名称。
+    bucket: "qrcode-portal",
+  });
 
-    // 获取下载exampleobject.txt文件的签名URL，使用浏览器访问时默认直接预览要下载的文件。
-    // 填写不包含Bucket名称在内的Object完整路径。
-    const url = client.signatureUrl(filename, {
-        expires: 1800,
-        method: "PUT",
-        "Content-Type": mimeString,
-    });
-    res.status(200).send(url);
+  // 获取下载exampleobject.txt文件的签名URL，使用浏览器访问时默认直接预览要下载的文件。
+  // 填写不包含Bucket名称在内的Object完整路径。
+  const url = client.signatureUrl(filename, {
+    expires: 1800,
+    method: "PUT",
+    "Content-Type": mimeString,
+  });
+  res.status(200).send(url);
 };
 
 exports.getSTSToken = async (req, res) => {
-    const params = {
-        RoleArn: "acs:ram::1299153890650559:role/sts-oss",
-        RoleSessionName: "test",
-        Policy: `{
+  const params = {
+    RoleArn: "acs:ram::1299153890650559:role/sts-oss",
+    RoleSessionName: "test",
+    Policy: `{
         "Version": "1",
         "Statement": [
             {
@@ -62,33 +63,33 @@ exports.getSTSToken = async (req, res) => {
             }
         ]
     }`,
-        DurationSeconds: 3600,
-    };
+    DurationSeconds: 3600,
+  };
 
-    var requestOption = {
-        method: "POST",
-    };
+  var requestOption = {
+    method: "POST",
+  };
 
-    var client = new Core({
-        accessKeyId: "LTAI5tCbNAQSZ2uAaz2jFtLk",
-        accessKeySecret: "H4evE0nAeJpkXsO6Q142Ssk4952ZAO",
-        endpoint: "https://sts.aliyuncs.com",
-        apiVersion: "2015-04-01",
-    });
+  var client = new Core({
+    accessKeyId: process.env.ALIYUN_ACCESS_KEY_ID,
+    accessKeySecret: process.env.ALIYUN_ACCESS_KEY_SECRET,
+    endpoint: "https://sts.aliyuncs.com",
+    apiVersion: "2015-04-01",
+  });
 
-    client.request("AssumeRole", params, requestOption).then(
-        (result) => {
-            res.status(200).json({
-                message: "get sts token successfully",
-                data: result,
-            });
-        },
-        (err) => {
-            res.status(500).json({
-                message: `failed to get sts token!, reason: ${err}`,
-            });
-        }
-    );
+  client.request("AssumeRole", params, requestOption).then(
+    (result) => {
+      res.status(200).json({
+        message: "get sts token successfully",
+        data: result,
+      });
+    },
+    (err) => {
+      res.status(500).json({
+        message: `failed to get sts token!, reason: ${err}`,
+      });
+    }
+  );
 };
 
 //构建一个阿里云客户端, 用于发起请求。
